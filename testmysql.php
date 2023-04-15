ecbe7a60d2b2aa0e8378d581c18e2fc92c243b86
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-15 09:33:19 +0100

    Create testmysql.php

---
+++ testmysql.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+$link = mysql_connect('hostname','dbuser','dbpassword'); 
+if (!$link) { 
+	die('Could not connect to MySQL: ' . mysql_error()); 
+} 
+echo 'Connection OK'; mysql_close($link); 
+?> 
